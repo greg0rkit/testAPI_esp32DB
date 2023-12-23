642cc159dbadfdc2b9fe1416145b365c675ed0bf
greg0rkit <gregorkit89@gmail.com>
2023-12-23 15:12:00 +0200

    changed sql string used ""

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def hello():
     date_now_formated = date_now.strftime("%d-%m-%Y")
     conn=psycopg2.connect(host="raspkit.duckdns.org", dbname="esp32", user="esp32", password="esp32pass")
     cur = conn.cursor()
-    cur.execute('SELECT temperaturevalue FROM TEMPERATURES WHERE DATE=(%s) ORDER BY DESC LIMIT 1', (date_now_formated))
+    cur.execute("SELECT temperaturevalue FROM TEMPERATURES WHERE DATE='(%s)' ORDER BY DESC LIMIT 1", (date_now_formated))
     result =cur.fetchall()
     cur.close()
     conn.close()
