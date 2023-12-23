bca40100a7db4bf5bafe48e06d6ed2bc9f5823d0
greg0rkit <gregorkit89@gmail.com>
2023-12-23 17:13:29 +0200

    new return string

--- app.py
+++ app.py
@@ -17,5 +17,7 @@ def hello():
     result_list = [r[0] for r in result]
     cur.close()
     conn.close()
-    return "Hello i am alive!! Last temperature reading was:".join(result[0])
+    return_string = "Hello i am alive!! Last temperature reading was:".join(result[0])
+    
+    return return_string
     
