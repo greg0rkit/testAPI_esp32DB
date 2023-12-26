61490b7e207b5b6b5fc2fd099b26649a8ac4dc01
greg0rkit <gregorkit89@gmail.com>
2023-12-26 14:01:08 +0200

    just a select all query

--- app.py
+++ app.py
@@ -13,18 +13,9 @@ app = Flask(__name__)
 def tempChart():
     conn=psycopg2.connect(host="raspkit.duckdns.org", dbname="esp32", user="esp32", password="esp32pass")
     cur = conn.cursor()
-    cur.execute("SELECT TIME FROM TEMPERATURES WHERE DATE='22-12-2023' ORDER BY TIME ASC")
+    cur.execute("SELECT * FROM TEMPERATURES")
     time_results = cur.fetchall()
     cur.close()
-    cur = conn.cursor()
-    cur.execute("SELECT TEMPERATUREVALUE FROM TEMPERATURES WHERE DATE='22-12-2023' ORDER BY TIME ASC")
-    temperature_results = cur.fetchall()
-    cur.close()
     conn.close()
-    time_results_list = [r[0] for r in time_results]
-    temperature_results_list = [float(r[0]) for r in temperature_results]
-    dict = {"Time":time_results_list, "Temperature":temperature_results_list}
-    df = pd.DataFrame(data=dict)
-    fig = px.line(df, x='Time', y='Temperature')
-    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-    return render_template('tempChart.html', graphJSON=graphJSON)
+    print(time_results)
+    return "debug"
